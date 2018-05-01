6db53dff99db21ded069658cce4796ae1f2d77ec
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-05-01 03:21:36 +0000

    finished Version 4, condensed and cleaned up code

--- todos.js
+++ todos.js
@@ -1,59 +1,26 @@
-// VERSION 1 & 2 COMPLETE
-
-var todos = ['item 1', 'item 2', 'item 3'];
-
-// It should have a function to display todos.
-function displayTodos() {
-    console.log('My todos:', todos)
-}
-
-// It should have a function to add todos.
-function addTodo(todo) {
-	todos.push(todo);
-	displayTodos();
-}
-
-// It should have a function to change todos.
-function changeTodo(position, newValue) {
-	todos[position] = newValue;
-	displayTodos();
-}
-
-// It should have a function to delete todos.
-function deleteTodo(position) {
-	todos.splice(position, 1)
-	displayTodos();
-}
-
-// VERSION 3 COMPLETE
-
-// It should store the todos array on an objects
 var todoList = {
-    
-  todos: ['item 1', 'item 2', 'item 3'],
-  
-  // It should have a dispayTodos method
+  todos: [],
   displayTodos: function() {
     console.log('My Todos:', this.todos);
   },
-  
-  // It should have an addTodo method
-  addTodo: function(todo) {
-    this.todos.push(todo);
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
     this.displayTodos();
   },
-  
-  // It should have a changeTodo method
-  changeTodo: function(position, newValue) {
-    this.todos[position] = newValue;
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText;
     this.displayTodos();
   },
-  
-  // It should have a deleteTodo method
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
     this.displayTodos();
+  },
+  toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
   }
 };
-
-// VERSION 4
